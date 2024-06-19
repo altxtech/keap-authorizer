60434b137ba847a44789b0d5687d837980f9c5b4
altxtech <andre.soares@altxtech.net>
2024-06-19 16:15:00 -0300

    Functionality to reset password, edit and delete users

--- src/keap_authorizer/users.py
+++ src/keap_authorizer/users.py
@@ -3,12 +3,21 @@ from flask import Blueprint, request, render_template, url_for, redirect
 from werkzeug.security import generate_password_hash
 from keap_authorizer.db import get_db
 from keap_authorizer.auth import auth
-import secrets
 
 bp = Blueprint("users", __name__, url_prefix="/users")
 
+def check_reset_password(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        user = auth.current_user()
+        if user.get("reset_password"):
+            return redirect(url_for("users.reset_password", username=user["username"]))
+        return view(**kwargs)
+    return wrapped_view
+
 @bp.route("/")
 @auth.login_required(role="admin")
+@check_reset_password
 def users():
     all_users = get_db().get_all_users()
     return render_template(
@@ -19,6 +28,7 @@ def users():
 
 @bp.route("/new-user", methods=["GET", "POST"])
 @auth.login_required(role="admin")
+@check_reset_password
 def new_user():
 
     if request.method == "GET":
@@ -49,6 +59,29 @@ def _create_user(username: str, password: str, email: str, roles: list[str]):
     }
     get_db().create_user(user)
 
+@bp.route("/<username>/edit", methods=["GET", "POST"])
+@auth.login_required(role="admin")
+@check_reset_password
+def edit_user(username: str):
+
+    if request.method == "GET":
+        user = get_db().get_user(username)
+        return render_template("edit-user-form.html", user=user)
+
+    email = request.form["email"]
+    roles = request.form["roles"].split(",")
+    get_db().update_user(username, {"email": email, "roles": roles})
+
+    return redirect(url_for("users.users"))
+
+@bp.route("/<username>/delete", methods=["POST"])
+@auth.login_required(role="admin")
+@check_reset_password
+def delete_user(username: str):
+    
+        get_db().delete_user(username)
+        return redirect(url_for("users.users"))
+
 # Reset password
 # Users can reset their own passwrods
 @bp.route("/<username>/reset-password", methods=["GET", "POST"])
@@ -74,21 +107,12 @@ def reset_password(username: str):
     if password != confirm_password:
         return render_template("reset-password-form.html", username=username, error="Passwords do not match")
 
-    # Update user
+    # If the password is being reset by and admin, the user will be required to reset their pasword
+    reset_password = is_admin
     updates = {
-        "password": generate_password_hash(username + ":" + password),
-        "reset_password": False
+        "password": generate_password_hash(password),
+        "reset_password": reset_password
     }
     get_db().update_user(username, updates)
 
     return redirect(url_for("main.integrations"))
-
-
-def check_reset_password(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        user = auth.current_user()
-        if user.get("reset_password"):
-            return redirect(url_for("users.reset_password", username=user["username"]))
-        return view(**kwargs)
-    return wrapped_view
